be3a43620eb9186f7129b5716e65a24badc6f2af
Gabriel Mors <gabrielmors210@gmail.com>
2023-06-09 20:38:02 -0400

    [FIX] adding the subViews, and, placing the button

--- ChatGPT/Chat/HomeScreen.swift
+++ ChatGPT/Chat/HomeScreen.swift
@@ -10,20 +10,18 @@ import UIKit
 class HomeScreen: UIView {
     
     lazy var tableView: UITableView = {
-       let tableView = UITableView()
+        let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.separatorStyle = .none
         tableView.showsVerticalScrollIndicator = false
         tableView.backgroundColor = .backGround
-        
         return tableView
     }()
     
     lazy var contentView: UIView = {
-       let view = UIView()
+        let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .backGround
-        
         return view
     }()
     
@@ -33,22 +31,52 @@ class HomeScreen: UIView {
         view.backgroundColor = .white
         view.clipsToBounds = true
         view.layer.cornerRadius = 8
-        
         return view
     }()
     
     lazy var messageTextField: UITextField = {
-       let textField = UITextField()
+        let textField = UITextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.autocorrectionType = .no
         textField.spellCheckingType = .no
         textField.keyboardType = .default
         textField.placeholder = "type here:"
         textField.borderStyle = .none
-        
         return textField
     }()
     
+    lazy var sendButton: UIButton = {
+        let button = UIButton ( )
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .darkBlue
+        button.isEnabled = false
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 8
+        button.setImage(UIImage (named: "send"), for: .normal)
+        button.addTarget (self, action: #selector (tappedSendButton), for: .touchUpInside)
+        return button
+    }()
     
+    @objc private func tappedSendButton() {
+        
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .backGround
+        addSubViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubViews() {
+        addSubview(tableView)
+        addSubview(contentView)
+        contentView.addSubview(subContentView)
+        subContentView.addSubview(messageTextField)
+        contentView.addSubview(sendButton)
+    }
     
 }
