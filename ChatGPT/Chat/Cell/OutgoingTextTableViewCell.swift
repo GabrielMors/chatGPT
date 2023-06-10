0688c95f59ec3266e0e2c5f7673eff6b6a4044e3
Gabriel Mors <gabrielmors210@gmail.com>
2023-06-10 17:15:25 -0400

    [FIX] created the outgoingcell elements

--- ChatGPT/Chat/Cell/OutgoingTextTableViewCell.swift
+++ ChatGPT/Chat/Cell/OutgoingTextTableViewCell.swift
@@ -11,10 +11,35 @@ class OutgoingTextTableViewCell: UITableViewCell {
     
     static let identefier: String = String(describing: OutgoingTextTableViewCell.self)
     
+    lazy var messageView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius  = 22
+        view.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner , .layerMaxXMinYCorner]
+        view.backgroundColor = .purple
+        return view
+    }()
     
+    lazy var messageLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: Font.fontMessage.rawValue, size: 16)
+        label.numberOfLines = 0
+        label.textColor = .white
+        return label
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .backGround
+        selectionStyle = .none
+        addElements()
+        
+    }
+    
+    private func addElements() {
+        contentView.addSubview(messageView)
+        messageView.addSubview(messageLabel)
     }
     
     required init?(coder: NSCoder) {
