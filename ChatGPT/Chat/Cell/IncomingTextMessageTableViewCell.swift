5af12e204a3c26d4c6f17cc8aa8787098c7be5c5
Gabriel Mors <gabrielmors210@gmail.com>
2023-06-10 17:06:01 -0400

    [FIX] created the cell elements

--- ChatGPT/Chat/Cell/IncomingTextMessageTableViewCell.swift
+++ ChatGPT/Chat/Cell/IncomingTextMessageTableViewCell.swift
@@ -7,12 +7,41 @@
 
 import UIKit
 
+enum Font: String {
+    case fontMessage = "HelveticaNeue-Medium"
+}
+
 class IncomingTextMessageTableViewCell: UITableViewCell {
 
     static let identefier: String = String(describing: IncomingTextMessageTableViewCell.self)
     
+    lazy var messageView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius  = 22
+        view.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner , .layerMaxXMinYCorner]
+        view.backgroundColor = .incomingColor
+        return view
+    }()
+    
+    lazy var messageLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: Font.fontMessage.rawValue, size: 16)
+        label.numberOfLines = 0
+        label.textColor = .white
+        return label
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addElements()
+        
+    }
+    
+    private func addElements() {
+        contentView.addSubview(messageView)
+        messageView.addSubview(messageLabel)
     }
     
     required init?(coder: NSCoder) {
