ac79c13117ce1ab228023280912ee90fc7bb4138
Gabriel Mors <gabrielmors210@gmail.com>
2023-06-10 17:08:32 -0400

    [FIX] created the cell constraints

--- ChatGPT/Chat/Cell/IncomingTextMessageTableViewCell.swift
+++ ChatGPT/Chat/Cell/IncomingTextMessageTableViewCell.swift
@@ -36,7 +36,7 @@ class IncomingTextMessageTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         addElements()
-        
+        configConstraints()
     }
     
     private func addElements() {
@@ -48,4 +48,18 @@ class IncomingTextMessageTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+            
+            messageView.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            messageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            messageView.widthAnchor.constraint(lessThanOrEqualToConstant: 250),
+            
+            messageLabel.topAnchor.constraint(equalTo: messageView.topAnchor, constant: 15),
+            messageLabel.trailingAnchor.constraint(equalTo: messageView.trailingAnchor, constant: -15),
+            messageLabel.leadingAnchor.constraint(equalTo: messageView.leadingAnchor, constant: 15),
+            messageLabel.bottomAnchor.constraint(equalTo: messageView.bottomAnchor, constant: -15),
+        ])
+    }
+    
 }
