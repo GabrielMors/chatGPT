7b00ec91f9702577acce331a417174711b8c126c
Gabriel Mors <gabrielmors210@gmail.com>
2023-06-11 14:23:14 -0400

    [NEW] new key

---
+++ ChatGPT/Secrets/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  ChatGPT
+//
+//  Created by Gabriel Mors  on 11/06/23.
+//
+
+import Foundation
+
+class APIKeys {
+    static let chatGPTKey = "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"
+}
